efd99def3afb26543ad8f6246a64e319e3f637fe
lisasamus <lisa_samus@mail.ru>
2021-06-09 10:18:48 +0300

    task fix

--- js/main.js
+++ js/main.js
@@ -15,14 +15,8 @@ const testTextLength = function (text, length) {
 };
 testTextLength('', 140);
 
-
-const description=[
-  'хорошо',
-  'плохо',
-  'розово',
-  'нормально',
-];
-const message=[
+const description = ['хорошо', 'плохо', 'розово', 'нормально'];
+const message = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -30,7 +24,7 @@ const message=[
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
 ];
-const names=[
+const names = [
   'Иван',
   'Хуан Себастьян',
   'Мария',
@@ -40,62 +34,53 @@ const names=[
   'Люпита',
   'Вашингтон',
 ];
-const avatar=[
-    'img/avatar-1.svg',
-    'img/avatar-2.svg',
-    'img/avatar-3.svg',
-    'img/avatar-4.svg',
-    'img/avatar-5.svg',
-    'img/avatar-6.svg',
-]
-const url=[
-    'photos/1.jpg',
-    'photos/2.jpg',
-    'photos/3.jpg',
-    'photos/5.jpg',
-    'photos/6.jpg',
-    'photos/7.jpg',
-    'photos/8.jpg',
-    'photos/9.jpg',
-    'photos/10.jpg',
-    'photos/11.jpg',
-    'photos/12.jpg',
-    'photos/13.jpg',
-    'photos/14.jpg',
-    'photos/15.jpg',
-    'photos/16.jpg',
-    'photos/17.jpg',
-    'photos/18.jpg',
-    'photos/19.jpg',
-    'photos/20.jpg',
-    'photos/21.jpg',
-    'photos/22.jpg',
-    'photos/23.jpg',
-    'photos/24.jpg',
-    'photos/25.jpg',
-]
 
-const getRandomArrayElement = (elements) => {
-    return elements[getRandomNumber(0, elements.length - 1)];
-  };
- 
-const createComment = () => {
-  return {
-    commentId: getRandomNumber(0, 25),
-    avatar: getRandomArrayElement(avatar),
-    message: getRandomArrayElement(message),
-    name: getRandomArrayElement(names),
-  };
-};
-const createPhoto= () => {
-    return {
-id:getRandomNumber(0, 25),
-url: getRandomArrayElement(url),
-description: getRandomArrayElement(description),
-likes: getRandomNumber(15, 200),
-createComment()
-    };
-};
-const object=[
-    createPhoto(), 
-]
+const url = [
+  'photos/1.jpg',
+  'photos/2.jpg',
+  'photos/3.jpg',
+  'photos/5.jpg',
+  'photos/6.jpg',
+  'photos/7.jpg',
+  'photos/8.jpg',
+  'photos/9.jpg',
+  'photos/10.jpg',
+  'photos/11.jpg',
+  'photos/12.jpg',
+  'photos/13.jpg',
+  'photos/14.jpg',
+  'photos/15.jpg',
+  'photos/16.jpg',
+  'photos/17.jpg',
+  'photos/18.jpg',
+  'photos/19.jpg',
+  'photos/20.jpg',
+  'photos/21.jpg',
+  'photos/22.jpg',
+  'photos/23.jpg',
+  'photos/24.jpg',
+  'photos/25.jpg',
+];
+
+const userPhotoСount = 25;
+
+const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
+
+const createComment = () => ({
+  commentId: getRandomNumber(0, 25),
+  avatar: `img/avatar-${getRandomNumber(0, 6)}.svg`,
+  message: getRandomArrayElement(message),
+  userName: getRandomArrayElement(names),
+});
+const createPhoto = () => ({
+  id: getRandomNumber(0, 25),
+  url: getRandomArrayElement(url),
+  description: getRandomArrayElement(description),
+  likes: getRandomNumber(15, 200),
+  comment: createComment(),
+});
+
+const userPhotos = new Array(userPhotoСount)
+  .fill(null)
+  .map(() => createPhoto());
+console.log(userPhotos);
